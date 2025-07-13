fe7a1d9fef592010c361047126234671bc3ac9c1
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2025-07-13 15:51:17 -0400

    v.2.10.0

--- code/metadata/definitionSet.js
+++ code/metadata/definitionSet.js
@@ -11,7 +11,7 @@ http://www.codeproject.com/Members/SAKryukov
 const definitionSet = {
 
     product: "Personal Database",
-    version: "2.9.20",
+    version: "2.10.0",
     years: "2017, 2023, 2025",
 
     productFormat: function() { return `${this.product} v.&thinsp;${this.version}`; },
